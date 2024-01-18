3137dda9ec8b51272fdf77647a1c12e31d90082d
MawlDonald8 <makimuramyu@gmail.com>
2024-01-18 23:37:38 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,17 +7,17 @@
 @section('content')
 
 <div class="w-100">
-
     <main class="form-signin w-100 m-auto">
         <form method="POST" action="{{ route('auth.login') }}" id="login-form">
-            <h1 class="h3 mb-3 fw-normal">Silahkan masuk untuk absensi</h1>
+            <h1 class="h2 mb-0 fw-normal text-center"><strong>ABSENSI</strong></h1>
+            <img src="{{ asset('images/inti.png') }}" alt="Description of the image" class="mx-auto d-block img-fluid mb-4" width="200" height="150">
 
-            <div class="form-floating">
+            <div class="form-floating mb-2">
                 <input type="email" class="form-control" id="floatingInputEmail" name="email"
                     placeholder="name@example.com">
                 <label for="floatingInputEmail">Email address</label>
             </div>
-            <div class="form-floating">
+            <div class="form-floating mb-2">
                 <input type="password" class="form-control" id="floatingPassword" name="password"
                     placeholder="Password">
                 <label for="floatingPassword">Password</label>
@@ -30,11 +30,10 @@
                 </label>
             </div>
 
-            <button class="w-100 btn btn-primary" type="submit" id="login-form-button">Masuk</button>
-            <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
+            <button class="w-50 d-flex justify-content-center mx-auto btn btn-primary" type="submit" id="login-form-button">Masuk</button>
+            <p class="mt-5 mb-3 text-muted text-right"></p>
         </form>
     </main>
-
 </div>
 @endsection
 
